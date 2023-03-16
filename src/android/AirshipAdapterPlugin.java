ea7cc24a9e4161a436b97c023cf3dcb3b9634b23
Kim Sparrow <kim.sparrow@gimbal.com>
2023-03-15 20:57:52 -0700

    feat: add Android configuration via preferences
    Add preferences for:
    * API key (Android)
    * auto-start
    * track place entry custom events (default enabled)
    * track place exit custom events (default enabled)
    * track region events (default disabled)
    
    Use new preferences in the plugin.  Apply `onload` to the plugin
    configuration so that the auto-start preference can take effect.

--- src/android/AirshipAdapterPlugin.java
+++ src/android/AirshipAdapterPlugin.java
@@ -1,21 +1,16 @@
 package com.gimbal.airship;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-import androidx.core.app.ActivityCompat;
-
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaPreferences;
 import org.apache.cordova.LOG;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class AirshipAdapterPlugin extends CordovaPlugin {
     private static final String TAG = "GIMBAL-AIRSHIP";
 
@@ -23,13 +18,26 @@ public class AirshipAdapterPlugin extends CordovaPlugin {
     private static final String ACTION_STOP = "stop";
 
     private AirshipAdapter adapter;
+    private Configuration config;
 
     @Override
     protected void pluginInitialize() {
         super.pluginInitialize();
 
+        config = new Configuration(preferences);
+
+        onUiThread(() -> {
+            getAdapter().setShouldTrackCustomEntryEvent(config.getTrackEntries());
+            getAdapter().setShouldTrackCustomExitEvent(config.getTrackExits());
+            getAdapter().setShouldTrackRegionEvent(config.getTrackRegionEvents());
+
+            if (config.getAutoStart() && config.getApiKey() != null && !getAdapter().isStarted()) {
+                start(config.getApiKey());
+            }
+        });
     }
 
+    @SuppressWarnings("RedundantThrows")
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
             throws JSONException {
@@ -58,32 +66,81 @@ public class AirshipAdapterPlugin extends CordovaPlugin {
     }
 
     private void start(JSONArray args, CallbackContext callbackContext) {
-        if ((args.length() != 1) && (args.optString(0, null) != null)) {
-            callbackContext.error("start: Expected single, non-null API Key string argument");
+        String apiKey;
+
+        if (args.length() != 1) {
+            callbackContext.error("Expected one nullable string Gimbal API Key argument");
+            return;
         }
-        String apiKey = args.optString(0);
-        if (ActivityCompat.checkSelfPermission(cordova.getContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            try {
-                boolean isStarted = getAdapter().start(apiKey);
-                callbackContext.sendPluginResult(
-                        new PluginResult(PluginResult.Status.OK, isStarted));
-            } catch (Exception e) {
-                LOG.e(TAG, "Failed to start Gimbal Airship adapter", e);
-                callbackContext.error(e.getMessage());
-            }
+        if (args.optString(0, null) != null) {
+            LOG.d(TAG, "start: Using API key from arguments");
+            apiKey = args.optString(0);
         } else {
-            callbackContext
-                    .error("Location permission is required to start Gimbal Airship adapter");
+            LOG.d(TAG, "start: Using API key from preferences");
+            apiKey = config.getApiKey();
+        }
+
+        if (apiKey == null || apiKey.isEmpty()) {
+            callbackContext.error("API Key must be non-null and non-empty");
+        }
+
+        try {
+            boolean isStarted = start(apiKey);
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, isStarted));
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private boolean start(String apiKey) {
+        try {
+            return getAdapter().start(apiKey);
+        } catch (Exception e) {
+            LOG.e(TAG, "start: Failed to start Gimbal Airship adapter", e);
+            throw e;
         }
     }
 
     private void stop(JSONArray args, CallbackContext callbackContext) {
         if (args.length() != 0) {
-            callbackContext.error("stop: Expected no arguments");
+            LOG.w(TAG, "stop: Expected no arguments");
         }
         getAdapter().stop();
         callbackContext.success();
     }
 
+    private static class Configuration {
+        private static final String GIMBAL_API_KEY = "com.gimbal.api_key.android";
+        private static final String GIMBAL_AUTO_START = "com.gimbal.auto_start";
+        private static final String GIMBAL_TRACK_ENTRIES = "com.gimbal.track_custom_events.entry";
+        private static final String GIMBAL_TRACK_EXITS = "com.gimbal.track_custom_events.exit";
+        private static final String GIMBAL_TRACK_REGION_EVENTS = "com.gimbal.track_region_events";
+
+        private final CordovaPreferences preferences;
+
+        Configuration(@NonNull CordovaPreferences preferences) {
+            this.preferences = preferences;
+        }
+
+        @Nullable
+        String getApiKey() {
+            return preferences.getString(GIMBAL_API_KEY, null);
+        }
+
+        boolean getAutoStart() {
+            return preferences.getBoolean(GIMBAL_AUTO_START, false);
+        }
+
+        boolean getTrackEntries() {
+            return preferences.getBoolean(GIMBAL_TRACK_ENTRIES, true);
+        }
+
+        boolean getTrackExits() {
+            return preferences.getBoolean(GIMBAL_TRACK_EXITS, true);
+        }
+
+        boolean getTrackRegionEvents() {
+            return preferences.getBoolean(GIMBAL_TRACK_REGION_EVENTS, false);
+        }
+    }
 }
--- www/AirshipAdapter.js
+++ www/AirshipAdapter.js
@@ -1,9 +1,46 @@
 var exec = require('cordova/exec');
 
+/**
+ * A success callback without arguments
+ *
+ * @callback successCallback
+ */
+
+/**
+ * A success callback that consumes a single boolean argument
+ *
+ * @callback booleanCallback
+ * @param {boolean} value
+ */
+
+/**
+ * An error callback without arguments
+ * 
+ * @callback errorCallback
+ */
+
+/**
+ * Starts the Airship Adapter with the specified Gimbal API key, or a key from Cordova preferences.
+ * Gimbal place events will trigger tracking of Airship CustomEvents or RegionEvents, if enabled
+ * via preferences.
+ * <p>
+ * Note that each of your Gimbal Apps for iOS and Android will have unique API keys.
+ * 
+ * @param {?string} apiKey - the Gimbal API key, or {@code null} to use Cordova preferences
+ * @param {booleanCallback} success - called upon successful start of the Airship Adapter.
+ *     The single boolean argument is whether the adapter started successfully.
+ * @param {errorCallback} error - called if this invocation fails
+ */
 exports.start = function(apiKey, success, error) {
-    exec(success, error, 'AirshipAdapter', 'start', [apiKey]);
+  exec(success, error, 'AirshipAdapter', 'start', [apiKey]);
 };
 
+/**
+ * Stops the Airship Adapter
+ * 
+ * @param {successCallback} success - called upon successful stop of the Airship Adapter.
+ * @param {errorCallback} error - called if this invocation fails
+ */
 exports.stop = function(success, error) {
     exec(success, error, 'AirshipAdapter', 'stop', []);
 };
