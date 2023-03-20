de71548e4f46bf752f4ee9995dc4eb9aa26ca2d4
Andrew Tran <andrew.tran@gimbal.com>
2023-03-20 14:36:51 -0700

    feat: add ios plugin platform
    Add iOS plugin classes.
    Specify minimum cordova-ios engine version for podspec functionality.
    Add iOS plugin platform data to plugin.xml.

--- src/ios/AirshipAdapterPlugin.m
+++ src/ios/AirshipAdapterPlugin.m
@@ -5,40 +5,81 @@
 @implementation AirshipAdapterPlugin
 
 -(void)pluginInitialize {
-  [super pluginInitialize];
-  [AirshipAdapter.shared restore];
+    [super pluginInitialize];
+    
+    NSString *preferenceApiKey = [self preferenceApiKey];
+    
+    AirshipAdapter.shared.shouldTrackCustomEntryEvents = [self shouldTrackCustomEntryEvents];
+    AirshipAdapter.shared.shouldTrackCustomExitEvents = [self shouldTrackCustomExitEvents];
+    AirshipAdapter.shared.shouldTrackRegionEvents = [self shouldTrackRegionEvents];
+    [AirshipAdapter.shared restore];
+    
+    if ([self shouldAutostart] && preferenceApiKey != nil && !AirshipAdapter.shared.isStarted) {
+        [self startWithApiKey:preferenceApiKey];
+    }
+}
+
+-(NSString*)preferenceApiKey {
+    return [self.commandDelegate.settings objectForKey:@"com.gimbal.api_key.ios"];
+}
+
+-(bool)shouldAutostart {
+    NSString* autostartConfigStringPreference = [[self.commandDelegate.settings
+                                                  objectForKey:@"com.gimbal.auto_start"] lowercaseString];
+    return autostartConfigStringPreference != nil && [autostartConfigStringPreference isEqualToString:@"true"];
+
+}
+
+-(bool)shouldTrackCustomEntryEvents {
+    NSString* shouldTrackEntryStringPreference = [[self.commandDelegate.settings
+                                                   objectForKey:@"com.gimbal.track_custom_events.entry"] lowercaseString];
+    return shouldTrackEntryStringPreference == nil || [shouldTrackEntryStringPreference isEqualToString:@"true"];
+}
+
+-(bool)shouldTrackCustomExitEvents {
+    NSString* shouldTrackExitStringPreference = [[self.commandDelegate.settings
+                                                   objectForKey:@"com.gimbal.track_custom_events.exit"] lowercaseString];
+    return shouldTrackExitStringPreference == nil || [shouldTrackExitStringPreference isEqualToString:@"true"];
+}
+
+-(bool)shouldTrackRegionEvents {
+    NSString* shouldTrackRegionStringPreference = [[self.commandDelegate.settings
+                                                   objectForKey:@"com.gimbal.track_region_events"] lowercaseString];
+    return shouldTrackRegionStringPreference != nil && [shouldTrackRegionStringPreference isEqualToString:@"true"];
 }
 
 -(void)start:(CDVInvokedUrlCommand *)command {
-  NSString *apiKey = [command argumentAtIndex:0];
-  CDVPluginResult *result;
-  if (apiKey == nil) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                               messageAsString:@"start: Expected single, non-null API Key string argument"];
-  } else if (![self hasActiveLocationServicesAuthorization]) {
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                               messageAsString:@"Location permission is required to start Gimbal Airship adapter"];
-  } else {
-    [AirshipAdapter.shared start:apiKey];
+    NSString *apiKey = [command argumentAtIndex:0];
+    CDVPluginResult *result;
+    if (apiKey == nil) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                   messageAsString:@"start: Expected single, non-null API Key string argument"];
+    } else if (![self hasActiveLocationServicesAuthorization]) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                   messageAsString:@"Location permission is required to start Gimbal Airship adapter"];
+    } else {
+        bool isStarted = [self startWithApiKey:apiKey];
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                     messageAsBool:isStarted];
+    }
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
 
-    BOOL isStarted = AirshipAdapter.shared.isStarted;
-    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                 messageAsBool:isStarted];
-  }
-  [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+-(bool)startWithApiKey:(NSString *)apiKey {
+    [AirshipAdapter.shared start:apiKey];
+    return AirshipAdapter.shared.isStarted;
 }
 
 -(void)stop:(CDVInvokedUrlCommand *)command {
-  [AirshipAdapter.shared stop];
-  CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    [AirshipAdapter.shared stop];
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
 -(BOOL)hasActiveLocationServicesAuthorization
 {
     CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
-    return (status == kCLAuthorizationStatusAuthorizedAlways)
-            || (status == kCLAuthorizationStatusAuthorizedWhenInUse);
+    return (status == kCLAuthorizationStatusAuthorizedAlways) || (status == kCLAuthorizationStatusAuthorizedWhenInUse);
 }
 
 @end
